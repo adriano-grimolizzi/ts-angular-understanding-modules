5b81dfaf07cf150997f154a0df12a948d4c64220
Adriano Grimolizzi <adriano.grimolizzi@amadeus.com>
2019-06-12 18:10:53 +0200

    fixed previous commit

--- file1.ts
+++
@@ -1,7 +0,0 @@
-// Export at declaration time
-export var immediate : string = "Exported Immediately!";
-
-// Export as a single statement
-var statement : string = "Exported as a statement...";
-
-export { statement }
--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
 // Import stylesheets
 import './style.css';
 
-// Da' errore, non so perche', ma funziona.
-import { immediate, statement } from './file1';
+// Angular uses MODULE RESOLUTION at compile-time to to find the 
+// intended module to import.
+// The .ts extension is not needed, as the TS is gonna be transpiled into JS.
+import { immediate, statement, duplicate } from './modules/module';
+
+// An ALIAS can be used when 2 different modules export a type with the same name.
+import { duplicate as duplicateAlias } from './modules/alias';
+
+// Or it's possible to import all types from a module into a variable.
+import * as types from './modules/importAll';
+
+// Rollup Exports into Barrels
+// ...
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 
 appDiv.innerHTML = 
     `<h1>` + immediate + `</h1>` +
-    `<h2>` + statement + `</h2>`;
+    `<h2>` + statement + `</h2>` +
+    `<h3>` + duplicate + `</h3>` +
+    `<h4>` + duplicateAlias + `</h4>` +
+    `<h5>` + types.duplicate + `</h5>`;
